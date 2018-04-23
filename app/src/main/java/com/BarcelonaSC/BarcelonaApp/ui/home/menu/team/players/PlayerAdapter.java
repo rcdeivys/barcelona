05da78f4b5d72a40e0e68d747c3053f1c8373f7a
leonardojpr <leonardojpr91@gmail.com>
2018-04-23 14:10:31 -0400

    Signed-off-by: leonardojpr <leonardojpr91@gmail.com>

--- app/src/main/java/com/BarcelonaSC/BarcelonaApp/ui/home/menu/team/players/PlayerAdapter.java
+++ app/src/main/java/com/BarcelonaSC/BarcelonaApp/ui/home/menu/team/players/PlayerAdapter.java
@@ -2,23 +2,26 @@ package com.BarcelonaSC.BarcelonaApp.ui.home.menu.team.players;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-import com.BarcelonaSC.BarcelonaApp.utils.ShareSection;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.BarcelonaSC.BarcelonaApp.R;
 import com.BarcelonaSC.BarcelonaApp.app.App;
 import com.BarcelonaSC.BarcelonaApp.models.GameSummonedData;
 import com.BarcelonaSC.BarcelonaApp.models.NominaItem;
+import com.BarcelonaSC.BarcelonaApp.ui.calendar.viewholder.MatchChildViewHolder;
 import com.BarcelonaSC.BarcelonaApp.utils.Commons;
 import com.BarcelonaSC.BarcelonaApp.utils.Constants.Constant;
-import com.BarcelonaSC.BarcelonaApp.utils.FCMillonariosTextView;
+import com.BarcelonaSC.BarcelonaApp.utils.ShareSection;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +33,7 @@ import butterknife.ButterKnife;
  * Created by Carlos on 11/10/2017.
  */
 
-public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
+public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
 
     private static final String TAG = PlayerAdapter.class.getSimpleName();
     private static final int TYPE_HEADER = 0;
@@ -40,6 +42,8 @@ public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     private static final String FINALIZED = "Finalizado";
     private Context context;
     private List<NominaItem> data;
+    private List<NominaItem> dataFiltered;
+    private List<NominaItem> oldData;
     private GameSummonedData gameSummonedData;
     private OnItemClickListener onItemClickListener;
     private boolean haveHeader = false;
@@ -48,19 +52,25 @@ public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         this.context = playerOffSummonedFragment.getContext();
         gameSummonedData = new GameSummonedData();
         data = new ArrayList<>();
-
+        oldData = new ArrayList<>();
         onItemClickListener = playerOffSummonedFragment;
     }
 
     public void setData(List<NominaItem> data) {
         haveHeader = false;
         this.data = data;
+        this.oldData.clear();
+        this.oldData.clear();
+        this.oldData.addAll(data);
+        this.dataFiltered = data;
     }
 
     public void setData(GameSummonedData gameSummonedData) {
         haveHeader = true;
         this.gameSummonedData = gameSummonedData;
         this.data = gameSummonedData.getJugadores();
+        this.oldData.addAll(gameSummonedData.getJugadores());
+        this.dataFiltered = data;
     }
 
     @Override
@@ -79,7 +89,7 @@ public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         if (viewType == TYPE_HEADER) {
-            return new VHHeader(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_team_game_header, parent, false));
+            return new MatchChildViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_match, parent, false));
         } else {
             return new VHItem(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_lv_players, parent, false));
         }
@@ -89,49 +99,35 @@ public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
 
-        if (holder instanceof VHHeader) {
-            VHHeader vhHeader = (VHHeader) holder;
-            vhHeader.teamOneName.setText(gameSummonedData.getEquipo1());
-            if (gameSummonedData.getBandera2() != null) {
-                Glide.with(context)
-                        .load(gameSummonedData.getBandera1())
-                        .into(vhHeader.teamOneFlag);
+        if (holder instanceof MatchChildViewHolder) {
+            MatchChildViewHolder matchChildViewHolder = (MatchChildViewHolder) holder;
+            matchChildViewHolder.tvMatchDate.setText(Commons.getStringDate(gameSummonedData.getFechaEtapa()));
 
+
+            Glide.with(context).load(gameSummonedData.getBandera1())
+                    .apply(new RequestOptions().placeholder(R.drawable.logo_millos).error(R.drawable.logo_millos))
+                    .into(matchChildViewHolder.ivTeamFlag1);
+
+            matchChildViewHolder.tvTeamName1.setText(gameSummonedData.getEquipo1());
+
+            Glide.with(context).load(gameSummonedData.getBandera2())
+                    .apply(new RequestOptions().placeholder(R.drawable.logo_millos).error(R.drawable.logo_millos))
+                    .into(matchChildViewHolder.ivTeamFlag2);
+
+            matchChildViewHolder.tvTeamName2.setText(gameSummonedData.getEquipo2());
+            if (Constant.Key.PROGRESS.equals(gameSummonedData.getEstado())) {
+                matchChildViewHolder.tvMatchScore.setText(gameSummonedData.getGoles1() + "-" + gameSummonedData.getGoles2());
+            } else if (Constant.Key.FINALIZED.equals(gameSummonedData.getEstado())) {
+                matchChildViewHolder.tvMatchScore.setText(gameSummonedData.getGoles1() + "-" + gameSummonedData.getGoles2());
+                matchChildViewHolder.tvMatchState.setText(gameSummonedData.getEstado().toUpperCase());
+                matchChildViewHolder.tvMatchState2.setText("");
+            } else {
+                matchChildViewHolder.tvMatchScore.setText(App.getAppContext().getString(R.string.vs));
             }
-            vhHeader.teamTwoName.setText(gameSummonedData.getEquipo2());
 
-            if (gameSummonedData.getBandera2() != null) {
-                Glide.with(context)
-                        .load(gameSummonedData.getBandera2())
-                        .into(vhHeader.teamTwoFlag);
+            if (gameSummonedData.getEstadio() != null && gameSummonedData.getFecha() != null && gameSummonedData.getEstado() != null) {
+                initDataGame(matchChildViewHolder, gameSummonedData);
             }
-
-
-            String result = gameSummonedData.getGoles1() + "-" + gameSummonedData.getGoles2();
-            if (gameSummonedData.getEstado().toUpperCase().equals(Constant.Key.PROGRESS) || gameSummonedData.getEstado().toUpperCase().equals(Constant.Key.FINALIZED))
-                vhHeader.clash.setText(result);
-            else
-                vhHeader.clash.setText("VS");
-
-            String[] arrayString = gameSummonedData.getFechaEtapa().split(",");
-            // vhHeader.gameType.setText(arrayString[1]);
-
-            vhHeader.ctv_stadium.setText(App.get().getString(R.string.stadium, gameSummonedData.getEstadio()));
-
-            vhHeader.ctvGameTime.setText(
-                    App.get().getString(R.string.game_date_time
-                            , Commons.getStringDate2(gameSummonedData.getFecha())
-                            , Commons.getStringHour(gameSummonedData.getFecha()))
-                            /*+ " (" + gameSummonedData.getEstado() + ")"*/);
-
-            vhHeader.dateFifa.setText(arrayString[0]);
-
-            vhHeader.llGame.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    onItemClickListener.onClickHeader();
-                }
-            });
 
         } else {
             VHItem vhItem = (VHItem) holder;
@@ -165,6 +161,37 @@ public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         }
     }
 
+    public void initDataGame(MatchChildViewHolder matchChildViewHolder, GameSummonedData match) {
+
+        if (match.getEstadio() != null) {
+            String state = App.get()
+                    .getString(R.string.stadium
+                            , match.getEstadio().replace("Estadio:", " "));
+
+            matchChildViewHolder.tvMatchState
+
+                    .setText(state);
+        }
+
+        if (match.getFecha() != null) {
+            String state2;
+            String date = Commons.getStringDate2(match.getFecha()).replace(".", "");
+            if (match.getInfo() != null) {
+                state2 = date.toUpperCase()
+                        + "  |  " + Commons.getStringHour(match.getFecha())
+                        + match.getInfo();
+            } else {
+                state2 = date.toUpperCase()
+                        + "  |  " + Commons.getStringHour(match.getFecha());
+            }
+
+            matchChildViewHolder.tvMatchState2
+                    .setText(state2);
+        }
+
+    }
+
+
     private NominaItem getItem(int position) {
         if (haveHeader)
             return data.get(position - 1);
@@ -178,6 +205,52 @@ public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             return data.size() + 1;
         else
             return data.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                Log.d(TAG, "charSequence " + charSequence);
+                if (charString.isEmpty()) {
+                    dataFiltered.clear();
+                    dataFiltered = oldData;
+                    Log.d(TAG, "filters oldData size " + oldData.size());
+                } else {
+                    List<NominaItem> filteredList = new ArrayList<>();
+                    for (NominaItem row : oldData) {
+
+                        if (row.getNombre().toUpperCase().contains(charString.toUpperCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    dataFiltered = filteredList;
+                }
+                Log.d(TAG, "filters size " + dataFiltered.size());
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = dataFiltered;
+                filterResults.count = dataFiltered.size();
+                Log.d(TAG, "filters size " + filterResults.count);
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                Log.d(TAG, "filters count " + filterResults.count);
+                data.clear();
+                data.addAll((ArrayList<NominaItem>) filterResults.values);
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public void closeSearch() {
+        data.clear();
+        data.addAll(oldData);
+        notifyDataSetChanged();
     }
 
     public interface OnItemClickListener {
@@ -199,33 +272,7 @@ public class PlayerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             super(view);
             ButterKnife.bind(this, view);
         }
-    }
 
-    class VHHeader extends RecyclerView.ViewHolder {
-        @BindView(R.id.team_one_name)
-        FCMillonariosTextView teamOneName;
-        @BindView(R.id.clash)
-        FCMillonariosTextView clash;
-        @BindView(R.id.team_two_name)
-        FCMillonariosTextView teamTwoName;
-        @BindView(R.id.ctv_stadium)
-        FCMillonariosTextView ctv_stadium;
-        @BindView(R.id.ctv_game_league)
-        FCMillonariosTextView ctvGameTime;
-        @BindView(R.id.date_fifa)
-        FCMillonariosTextView dateFifa;
-        @BindView(R.id.team_one_flag)
-        ImageView teamOneFlag;
-        @BindView(R.id.team_two_flag)
-        ImageView teamTwoFlag;
-        @BindView(R.id.LL_Game)
-        LinearLayout llGame;
-
-
-        VHHeader(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-        }
     }
 
 }
